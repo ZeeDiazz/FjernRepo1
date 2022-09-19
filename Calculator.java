b02c0fb414be5c1082d5036fe1cbfa5bda3dba4c
Zigalow <s224302@dtu.dk>
2022-09-19 15:13:21 +0200

    try1

--- Calculator.java
+++ Calculator.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
+        System.out.println("Zahedullah er helt død"); //jk mate
+
 
         char calculate;
         Double num, num2, result;
